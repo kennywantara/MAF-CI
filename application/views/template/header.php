ceb9553512cc81e8701f9170622f41b53a26ba7e
nathaniaelvina <nathaniaelvina@hotmail.com>
2017-05-10 20:56:31 +0700

    icon shopping cart gagal

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -5,7 +5,7 @@
         </div>        
         <div class="top-nav">
             <ul class="memenu">
-                <li class=""><a href="index.php">Home</a></li>
+                <li class=""><a href="<?php echo site_url("Home/index"); ?>">Home</a></li>
                 <li class=""><a href="<?php echo site_url("Products/index"); ?>">Products</a></li>
                 <li class=""><a href="<?php echo site_url("Contact/index"); ?>">Contact</a></li>               
                 
@@ -13,6 +13,7 @@
         </div>
         <div class="top-nav"  style="float:right; width:40%;">
             <ul class="memenu" >
+                <li><a style="float:right;" href="" class="glyphicon glyphicon-shopping-cart"></a>  
                 <li class="" ><a style="float:right;" href="<?php echo site_url("SignUp/index"); ?>">Sign up</a></li>
                 <li class="" ><a style="float:right;" href="<?php echo site_url("SignIn/index"); ?>">Login</a></li>
             </ul>               
