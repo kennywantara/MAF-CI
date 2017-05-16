b13fe2ff57ebca9b91740092411361d32f9c0067
nathaniaelvina <nathaniaelvina@hotmail.com>
2017-05-16 18:29:35 +0700

    admin

--- application/views/admin.php
+++ application/views/admin.php
@@ -12,11 +12,17 @@ foreach($css_files as $file): ?>
 <?php endforeach; ?>
 </head>
 <body>
+	<div class="row">
+		<div class="col-md-3">
+			<ul class="nav nav-pills nav-stacked">
+			  <li class="active"><a href='<?php echo site_url('admin/customers_management')?>'>Customers</a></li>
+			  <li><a href='<?php echo site_url('admin/orders_management')?>'>Orders</a></li>
+			  <li><a href='<?php echo site_url('admin/products_management')?>'>Products</a></li>
+			  <li><a href='<?php echo site_url('admin/orderdetails_management')?>'>Order Details</a></li>
+			</ul>
+		</div>
+	</div>
 	<div>
-		<a href='<?php echo site_url('admin/customers_management')?>'>Customers</a> |
-		<a href='<?php echo site_url('admin/orders_management')?>'>Orders</a> |
-		<a href='<?php echo site_url('admin/products_management')?>'>Products</a> |
-		<a href='<?php echo site_url('admin/orderdetails_management')?>'>Order Details</a> | 
 		
 	</div>
 	<div style='height:20px;'></div>  
